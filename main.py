4a0f037100c481f941c7fe374215a1e28ae26afd
Vladislav <126583090+VladislavXD@users.noreply.github.com>
2025-05-03 15:57:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 
 # connect DB + create bot
 db = Database('english.db')
-bot = telebot.TeleBot(token='6811304180:AAEP8sZtqEOBl7f1PfaSFIfi0YFg_X7L2Os')) 
+bot = telebot.TeleBot(token=)) 
 bot.remove_webhook()
 ADMIN_ID = 964928426
 
